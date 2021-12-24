fa79fbbec8b459b839d5d37bd613a7f60083ba11
bussury <bussury.omary@gmail.com>
2021-12-24 17:07:54 +0300

    entry

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <main>
     <div class="px-4 py-6 sm:px-0">
       <div
-        class="p-4 text-center text-gray-400 border-4 border-gray-200 border-dashed rounded-lg  h-96"
+        class="p-4 text-center text-gray-600 border-4 border-gray-200 border-dashed rounded-lg  h-96"
       >
         Here goes your content. You can also go the
         <router-link
