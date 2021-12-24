38d678b36aad30f334447e299a7ff21396c8bf9c
bussury <bussury.omary@gmail.com>
2021-12-24 16:11:17 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import { createApp } from 'vue'
+import './assets/main.css'
+import App from './App.vue'
+import { routes } from './routes.js'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createHead } from '@vueuse/head'
+
+let app = createApp(App)
+let router = createRouter({
+  history: createWebHistory(),
+  routes: import.meta.hot ? [] : routes,
+})
+const head = createHead()
+
+if (import.meta.hot) {
+  let removeRoutes = []
+
+  for (let route of routes) {
+    removeRoutes.push(router.addRoute(route))
+  }
+
+  import.meta.hot.accept('./routes.js', ({ routes }) => {
+    for (let removeRoute of removeRoutes) removeRoute()
+    removeRoutes = []
+    for (let route of routes) {
+      removeRoutes.push(router.addRoute(route))
+    }
+    router.replace('')
+  })
+}
+
+app.use(router)
+app.use(head)
+app.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div class="px-4 py-6 sm:px-0">
+      <div
+        class="p-4 text-center text-gray-400 border-4 border-gray-200 border-dashed rounded-lg  h-96"
+      >
+        Here goes your content. You can also go the
+        <router-link
+          to="/about"
+          class="text-indigo-600 underline hover:text-indigo-500"
+          >About page</router-link
+        >.
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { useHead } from '@vueuse/head'
+
+  useHead({
+      title: 'Vite:Home',
+      meta: [
+      {
+        name: 'description',
+        content: 'vite home ',
+      }]
+    })
+</script>
