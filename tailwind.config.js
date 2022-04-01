ec5a6e15232bb666268084786ca7036c7ff1f05a
bussury <bussury.omary@gmail.com>
2022-04-01 11:53:23 +0300

    update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   mode: 'jit',
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    // classes that are generated dynamically, e.g. `rounded-${size}` and must
-    // be kept
-    safeList: [],
-    content: [
-      './index.html',
-      './src/**/*.{vue,js,ts}',
-      // etc.
-    ],
-  },
+  enabled: process.env.NODE_ENV === 'production',
+  safeList: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts}',
+    // etc.
+  ],
   theme: {
     extend: {
       fontFamily: {
