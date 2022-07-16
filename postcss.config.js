d695bc91d2073e12cbf80fd65721c2d4edf1ea8a
bussury <bussury.omary@gmail.com>
2022-07-16 10:54:56 +0300

    al change added for better performance

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,11 @@
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
-};
+// export const plugins = {
+//   tailwindcss: {},
+//   autoprefixer: {},
+// };
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
-import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme'
+const defaultTheme = require('tailwindcss/defaultTheme')
 
-export const mode = 'jit'
-export const enabled = process.env.NODE_ENV === 'production'
-export const safeList = []
-export const content = [
-  './index.html',
-  './src/**/*.{vue,js,ts}',
-  // etc.
-]
-export const theme = {
-  extend: {
-    fontFamily: {
-      sans: ['Inter var', ..._fontFamily.sans],
+module.exports = {
+  mode: 'jit',
+  enabled: process.env.NODE_ENV === 'production',
+  safeList: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts}',
+    // etc.
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
 }
