1f89f11e34fb574858edbc0c3ccd8d0a28f657c6
bussury <bussury.omary@gmail.com>
2022-07-16 10:54:32 +0300

    change server name

---
+++ server.mjs
@@ -0,0 +1,39 @@
+import fastify from 'fastify'
+import middiePlugin from '@fastify/middie';
+
+import {viteSSR} from './core/ViteSSR.mjs';
+
+async function main() {
+  const app = fastify({
+    logger: false
+  })
+  await app.register(middiePlugin);
+  await app.register(viteSSR)
+  
+ /**
+   * Load all api routes
+   */
+  app.get('/api/users', async (req, res) =>{
+    return res.send([
+      {"name":"John", "age":30, "car":'morano'},
+      {"name":"Bussury", "age":33, "car":null},
+    ])
+  })
+  app.get('/api/persons', async (req, res ) => {
+    return res.send(
+      [
+        {'name':'Ally', 'username':'all'},
+        {'name':'Kesi', 'username':'kesi'},
+        {'name':'James', 'username':'james'},
+      ]
+    )
+  })
+  return app
+}
+if (!process.argv.includes('test')) {
+  const app = await main()
+  const address = await app.listen({port: 3010})
+  console.log(`Listening at ${address}.`)
+}
+
+export default main
