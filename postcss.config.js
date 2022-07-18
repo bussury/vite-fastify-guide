169de9760c4aee4f31a4111df89e97c00dfb5a02
bussury <bussury.omary@gmail.com>
2022-07-18 22:38:20 +0300

    postcss setting

--- postcss.config.js
+++ postcss.config.js
@@ -6,14 +6,3 @@ import tailwindConfig from './tailwind.config.cjs'
 export default {
   plugins: [tailwind(tailwindConfig), autoprefixer],
 }
-// export const plugins = {
-//   tailwindcss: {},
-//   autoprefixer: {},
-// };
-
-// // module.exports = {
-// //   plugins: {
-// //     tailwindcss: {},
-// //     autoprefixer: {},
-// //   },
-// // }
