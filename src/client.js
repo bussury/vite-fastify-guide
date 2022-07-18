af25aa69b18ed92d97f8f1684b3033752425f6e3
bussury <bussury.omary@gmail.com>
2022-07-18 13:41:48 +0300

    remove .mjs

--- src/client.js
+++ src/client.js
@@ -15,9 +15,9 @@ export async function createApp(ctx, url){
   app.use(router)
   app.use(head)
 
-  // if (url) {
-  //   router.push(url)
-  //    router.isReady()
-  // }
+  if (url) {
+    router.push(url)
+     router.isReady()
+  }
   return { ctx, app, head, router }
 }
