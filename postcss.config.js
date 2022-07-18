7260093bdf6e3112159173330030c77fb840e40b
bussury <bussury.omary@gmail.com>
2022-07-18 13:33:10 +0300

    postfix config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,19 @@
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import tailwindConfig from './tailwind.config.cjs'
+
+
+export default {
+  plugins: [tailwind(tailwindConfig), autoprefixer],
+}
 // export const plugins = {
 //   tailwindcss: {},
 //   autoprefixer: {},
 // };
 
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// // module.exports = {
+// //   plugins: {
+// //     tailwindcss: {},
+// //     autoprefixer: {},
+// //   },
+// // }
